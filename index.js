4153a8001805d63f9d291af4980c6dcbae83f381
agubucu <aemirozbek@gmail.com>
2023-08-14 00:18:17 +0300

    scale switch for mapping alignment and format doc

--- index.js
+++ index.js
@@ -10,38 +10,38 @@ const marginSides = 60;
 const legendWidth = 400;
 
 const heatmapColors = {
-    0: '#0000FF',  // Blue
-    1: '#1E46A0',
-    2: '#3C7E7F',
-    3: '#5ABD5B',
-    4: '#79FC38',
-    5: '#9BF91A',  // Yellow
-    6: '#B8D300',
-    7: '#D59B00',
-    8: '#F27400',
-    9: '#FF4200',
-    10: '#FF0000'  // Red
-  };
+  0: "#0000FF", // Blue
+  1: "#1E46A0",
+  2: "#3C7E7F",
+  3: "#5ABD5B",
+  4: "#79FC38",
+  5: "#9BF91A", // Yellow
+  6: "#B8D300",
+  7: "#D59B00",
+  8: "#F27400",
+  9: "#FF4200",
+  10: "#FF0000", // Red
+};
 
 fetch(url)
   .then((res) => res.json())
   .then((data) => {
     const formatMonth = d3.timeFormat("%B");
     const formatYear = d3.timeFormat("%Y");
-    const dataset = data.monthlyVariance
-    const baseTemp = data.baseTemperature
-    const variance = dataset.map(e => e.variance)
-    const years = dataset.map(e => new Date(e.year, 0))
-    const yearsAmount = yearsAmountFinder(years)
+    const dataset = data.monthlyVariance;
+    const baseTemp = data.baseTemperature;
+    const variance = dataset.map((e) => e.variance);
+    const years = dataset.map((e) => new Date(e.year, 0));
+    const yearsAmount = yearsAmountFinder(years);
     let months = [];
-    for(let i = 0; i < 12; i++){
-        months.push(i)
+    for (let i = 0; i < 12; i++) {
+      months.push(i);
     }
 
     function yearsAmountFinder(input) {
-      let arr = input.map(e => e.getFullYear())
-      arr = new Set(arr)
-      return arr.size
+      let arr = input.map((e) => e.getFullYear());
+      arr = new Set(arr);
+      return arr.size;
     }
 
     const xScale = d3
@@ -50,8 +50,8 @@ fetch(url)
       .range([marginSides, w - marginSides]);
 
     const yScale = d3
-      .scaleLinear()
-      .domain([d3.max(months), d3.min(months)])
+      .scaleBand()
+      .domain([11,10,9,8,7,6,5,4,3,2,1,0])
       .range([h - marginBottom, marginTop]);
 
     const svg = d3
@@ -74,12 +74,12 @@ fetch(url)
       .enter()
       .append("rect")
       .attr("class", "cell")
-      .attr("data-month", (d) => (d.month - 1))
+      .attr("data-month", (d) => d.month - 1)
       .attr("data-year", (d) => d.year)
-      .attr("data-temp", (d) => (d.variance + data.baseTemperature))
+      .attr("data-temp", (d) => d.variance + data.baseTemperature)
       .attr("x", (d) => xScale(new Date(d.year + "-1")))
       .attr("y", (d) => yScale(d.month - 1))
-      .attr("width", (w - (2*marginSides)) / yearsAmount)
+      .attr("width", (w - 2 * marginSides) / yearsAmount)
       .attr("height", (h - marginTop - marginBottom) / months.length)
       .attr("fill", "red");
 
@@ -93,7 +93,9 @@ fetch(url)
       .append("g")
       .attr("transform", "translate(" + marginSides + ", 0)")
       .attr("id", "y-axis")
-      .call(d3.axisLeft(yScale).tickFormat(d => formatMonth(new Date(2000, d))))
+      .call(
+        d3.axisLeft(yScale).tickFormat((d) => formatMonth(new Date(2000, d)))
+      );
 
     svg
       .append("text")
@@ -121,9 +123,7 @@ fetch(url)
       .attr("text-anchor", "middle")
       .text("Months");
 
-    const legendBox = svg
-      .append("g")
-      .attr("id", "legend")
+    const legendBox = svg.append("g").attr("id", "legend");
 
     const legendX = d3
       .scaleLinear()
@@ -132,20 +132,32 @@ fetch(url)
 
     legendBox
       .append("g")
-      .attr("transform", "translate(0, " + (h+30) + ")")
+      .attr("transform", "translate(0, " + (h + 30) + ")")
       .attr("id", "legend-x")
       .call(d3.axisBottom(legendX));
 
     rects.on("mouseover", (event, d) => {
       const monthNames = [
-        "January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
       ];
       const f = d3.format(".1f");
       tooltip.transition().duration(200).style("opacity", 1);
       tooltip
         .html(
-          `${d.year} - ${monthNames[d.month-1]}<br/>${f(d.variance + baseTemp)}&deg;C<br/>${f(d.variance)}&deg;C`
+          `${d.year} - ${monthNames[d.month - 1]}<br/>${f(
+            d.variance + baseTemp
+          )}&deg;C<br/>${f(d.variance)}&deg;C`
         )
         .style("left", event.pageX + 15 + "px")
         .style("top", event.pageY + "px")
